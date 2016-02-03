c7c5776ee6ad546e5eeebc44dcae6801f9fe91e2
Edvin Salo <CodeGeek@Edvins-Mac.local>
2016-02-03 12:26:14 +0100

    customized filter added

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,13 @@
 angular.module('FlFeederApp.controllers', []).
 controller('driversController', function($scope, ergastAPIService) {
-  $scope.nameFilter = null;
+  $scope.nameFilter = '';
 	$scope.driversList = [];
+
+  $scope.searchFilter = function(driver) {
+    var keyword = new RegExp($scope.nameFilter, 'i');
+    return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || 
+      keyword.test(driver.Driver.familyName);
+  };
 
   ergastAPIService.getDrivers().success(function(response) {
     $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
